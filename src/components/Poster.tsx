f4529566d6f5aa6946b42d162b3cb560eddc1388
cubeliu <cuberjhcubing@gmail.com>
2026-06-13 15:19:51 +0800

    add poster tab

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -1,13 +1,26 @@
+import poster1 from '../assets/poster1.png';
+import poster2 from '../assets/poster2.png';
+
 export default function Poster() {
   return (
-    <div>
+    <div className="space-y-6">
       <h1 className="text-3xl font-bold">
         testing
       </h1>
 
-      <p className="mt-4">
-        testing
-      </p>
+      <div className="grid md:grid-cols-2 gap-4">
+        <img
+          src={poster1}
+          alt="海報1"
+          className="rounded-lg shadow-lg"
+        />
+
+        <img
+          src={poster2}
+          alt="海報2"
+          className="rounded-lg shadow-lg"
+        />
+      </div>
     </div>
   );
 }
